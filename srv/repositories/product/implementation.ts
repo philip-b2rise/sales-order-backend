36d3f0bea35c16843b03b8aefc23719af87ac240
Philip Silva e Lages <psilvaelages@gmail.com>
2025-05-08 13:55:22 -0300

    feat: implement ProductRepository interface and ProductRepositoryImpl for product data access

---
+++ srv/repositories/product/implementation.ts
@@ -0,0 +1,23 @@
+import cds from '@sap/cds';
+
+import { Products } from '@models/sales';
+import { ProductModel, ProductProps } from "srv/models/product";
+import { ProductRepository } from "./protocols";
+
+export class ProductRepositoryImpl implements ProductRepository {
+    public async findByIds(ids: ProductProps['id'][]): Promise<ProductModel[] | null> {
+        const productsQuery = SELECT.from('sales.Products').where({ id: ids });
+        const products: Products = await cds.run(productsQuery);
+
+        if (!products.length) {
+            return null;
+        }
+
+        return products.map((product) => ProductModel.with({
+            id: product.id as string,
+            name: product.name as string,
+            price: product.price as number,
+            stock: product.stock as number,
+        }));
+    }
+}
