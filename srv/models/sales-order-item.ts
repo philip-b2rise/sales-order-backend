6400e73e07a82b7f8165df43ed29fd1c43aa90d2
Philip Silva e Lages <psilvaelages@gmail.com>
2025-05-08 14:59:24 -0300

    feat: add SalesOrderItemModel

---
+++ srv/models/sales-order-item.ts
@@ -0,0 +1,68 @@
+import { ProductModel } from "./product";
+
+export type SalesOrderItemProps = {
+    id: string;
+    productId: string;
+    quantity: number;
+    price: number;
+    products: ProductModel[];
+}
+
+type CreationPayload = {
+    product_id: SalesOrderItemProps['productId'];
+}
+
+type CreationPayloadValidationResult = {
+    hasError: boolean;
+    error?: Error;
+}
+
+export class SalesOrderItemModel {
+    constructor(private props: SalesOrderItemProps) {}
+
+    public static with(props: SalesOrderItemProps) {
+        return new SalesOrderItemModel(props);
+    }
+
+    public get id() {
+        return this.props.id;
+    }
+
+    public get productId() {
+        return this.props.productId;
+    }
+
+    public get quantity() {
+        return this.props.quantity;
+    }
+
+    public get price() {
+        return this.props.price;
+    }
+
+    public get products() {
+        return this.props.products;
+    }
+
+    public validateCreationPayload(payload: CreationPayload): CreationPayloadValidationResult {
+        const product = this.products.find((product) => product.id === payload.product_id);
+
+        if (!product) {
+            return {
+                hasError: true,
+                error: new Error(`Product ${payload.product_id} not found`)
+            }
+        }
+
+        if (product.stock === 0) {
+            return {
+                hasError: true,
+                error: new Error(`Product ${product.name} (${product.id}) out of stock`)
+            }
+        }
+
+        return {
+            hasError: false
+        }
+    }
+}
