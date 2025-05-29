d7f79e694aede4a2c95bac48d7f57498fef49cd0
Philip Silva e Lages <psilvaelages@gmail.com>
2025-05-29 15:14:31 -0300

    fix: add check for email presence in setDefaultEmailDomain method to prevent unnecessary processing

--- srv/models/customer.ts
+++ srv/models/customer.ts
@@ -29,6 +29,10 @@ export class CustomerModel {
     }
 
     public setDefaultEmailDomain(): CustomerModel {
+        if (!this.props.email) {
+            return this;
+        }
+
         if (!this.props.email?.includes('@')) {
             this.props.email = `${this.props.email}@sap.com`;
         }
