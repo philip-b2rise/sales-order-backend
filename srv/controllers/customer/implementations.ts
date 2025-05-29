28bde4644825f64becf4f973a6e15ab8e1794c85
Philip Silva e Lages <psilvaelages@gmail.com>
2025-05-29 09:48:02 -0300

    refactor: extend CustomerControllerImpl from BaseControllerImpl and update afterRead method

--- srv/controllers/customer/implementations.ts
+++ srv/controllers/customer/implementations.ts
@@ -1,12 +1,22 @@
 import { Customers } from '@models/sales';
 
+import { BaseControllerImpl } from '@/controllers/base/implementation';
+import { BaseControllerResponse } from '@/controllers/base/protocol';
 import { CustomerController } from '@/controllers/customer/protocols';
 import { CustomerService } from '@/services/customer/protocols';
 
-export class CustomerControllerImpl implements CustomerController {
-    constructor(private readonly service: CustomerService) {}
+export class CustomerControllerImpl extends BaseControllerImpl implements CustomerController {
+    constructor(private readonly service: CustomerService) {
+        super();
+    }
 
-    public afterRead(customerList: Customers): Customers {
-        return this.service.afterRead(customerList);
+    public afterRead(customerList: Customers): BaseControllerResponse {
+        const result = this.service.afterRead(customerList);
+
+        if (result.isLeft()) {
+            return this.error(result.value.code, result.value.message);
+        }
+
+        return this.success(result.value);
     }
 }
