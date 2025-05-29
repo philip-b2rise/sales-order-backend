21267f4c9cff8b63b2c77d5d5ea933673fe8d019
Philip Silva e Lages <psilvaelages@gmail.com>
2025-05-29 09:47:27 -0300

    refactor: update afterRead method to return Either type for error handling

--- srv/services/customer/implementation.ts
+++ srv/services/customer/implementation.ts
@@ -1,20 +1,28 @@
-import { CustomerModel } from '@/models/customer';
-import { CustomerService } from '@/services/customer/protocols';
+import { Either, left, right } from '@sweet-monads/either';
+
 import { Customers } from '@models/sales';
 
+import { CustomerModel } from '@/models/customer';
+import { CustomerService } from '@/services/customer/protocols';
+import { AbstractError, ServerError } from '@/errors';
+
 export class CustomerServiceImpl implements CustomerService {
-    public afterRead(customerList: Customers): Customers {
-        const customers = customerList.map((customer) => {
-            const customerData = CustomerModel.with({
-                id: customer.id as string,
-                firstName: customer.firstName as string,
-                lastName: customer.lastName as string,
-                email: customer.email as string
+    public afterRead(customerList: Customers): Either<AbstractError, Customers> {
+        try {
+            const customers = customerList.map((customer) => {
+                const customerData = CustomerModel.with({
+                    id: customer.id as string,
+                    firstName: customer.firstName as string,
+                    lastName: customer.lastName as string,
+                    email: customer.email as string
+                });
+
+                return customerData.setDefaultEmailDomain().toObject();
             });
-
-            return customerData.setDefaultEmailDomain().toObject();
-        });
-
-        return customers;
+            return right(customers);
+        } catch (error) {
+            const errorInstance: Error = error as Error;
+            return left(new ServerError(errorInstance.stack as string, errorInstance.message));
+        }
     }
 }
--- srv/services/customer/protocols.ts
+++ srv/services/customer/protocols.ts
@@ -1,5 +1,9 @@
+import { Either } from '@sweet-monads/either';
+
 import { Customers } from '@models/sales';
 
+import { AbstractError } from '@/errors';
+
 export interface CustomerService {
-    afterRead(customerList: Customers): Customers;
+    afterRead(customerList: Customers): Either<AbstractError, Customers>;
 }
