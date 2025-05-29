b2a247d745f357cd8e3fca0ff777aa81ce321cf3
Philip Silva e Lages <psilvaelages@gmail.com>
2025-05-29 10:29:43 -0300

    refactor: extend SalesReportControllerImpl from BaseControllerImpl

--- srv/controllers/sales-report/implementation.ts
+++ srv/controllers/sales-report/implementation.ts
@@ -1,13 +1,23 @@
 import { ExpectedResult as SalesReportByDays } from '@models/db/types/SalesReportByDays';
 
+import { BaseControllerImpl } from '@/controllers/base/implementation';
+import { BaseControllerResponse } from '@/controllers/base/protocol';
 import { SalesReportController } from '@/controllers/sales-report/protocols';
 import { SalesReportService } from '@/services/sales-report/protocol';
 
-export class SalesReportControllerImpl implements SalesReportController {
-    constructor(private readonly service: SalesReportService) {}
+export class SalesReportControllerImpl extends BaseControllerImpl implements SalesReportController {
+    constructor(private readonly service: SalesReportService) {
+        super();
+    }
 
-    public async findByDays(days: number): Promise<SalesReportByDays[] | null> {
-        return this.service.findByDays(days);
+    public async findByDays(days: number): Promise<BaseControllerResponse> {
+        const result = await this.service.findByDays(days);
+
+        if (result.isLeft()) {
+            return this.error(result.value.code, result.value.message);
+        }
+
+        return this.success(result.value);
     }
 
     public async findByCustomerId(customerId: string): Promise<SalesReportByDays[] | null> {
--- srv/controllers/sales-report/protocols.ts
+++ srv/controllers/sales-report/protocols.ts
@@ -1,6 +1,7 @@
+import { BaseControllerResponse } from '@/controllers/base/protocol';
 import { ExpectedResult as SalesReportByDays } from '@models/db/types/SalesReportByDays';
 
 export interface SalesReportController {
-    findByDays(days: number): Promise<SalesReportByDays[] | null>;
+    findByDays(days: number): Promise<BaseControllerResponse>;
     findByCustomerId(customerId: string): Promise<SalesReportByDays[] | null>;
 }
